6adb131b1c914c23ebb2ef26a139cb8dba77a53b
anushaande <andeanusha24@gmail.com>
2024-08-20 12:50:22 -0400

    adding d11

---
+++ d11_number_guessing game/number_guessing_game.py
@@ -0,0 +1,30 @@
+import random
+
+NUM = random.randint(1,100)
+game_on = True
+
+level = input("Choose a difficulty. Type 'easy' or 'hard': ")
+if level == 'easy':
+    attempts = 10
+elif level == "hard":
+    attempts = 5
+else:
+    print("level should only be 'easy' or 'hard'. Please check the spelling and try again.")
+    game_on = False
+
+while game_on:
+    print(f"You have {attempts} attempts remaining to guess the number")
+    n = int(input("Make a guess: "))
+    if n < NUM :
+        print("Too Low.")
+        game_on = True
+    elif n > NUM:
+        print("Too High.")
+        game_on = True
+    elif n == NUM:
+        print(f"You got it! The answer was {NUM}")
+        game_on = False
+    print("Guess_again.")
+    attempts -= 1
+
+
